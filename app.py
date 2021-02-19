7f37fba0c417b1133c420c1c03105d0fd2b0b2cb
Marlon Beck <thx4nothing15@gmail.com>
2021-02-19 12:46:00 +0100

    gradually increase overlay effect

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-import logging
 import time
 import edgeiq
 from posture import CheckPosture
 import tkinter, win32api, win32con, pywintypes
 from PIL import Image, ImageTk
-from posture import CheckPosture
+
 """
 Use pose estimation to determine human poses in realtime. Human Pose returns
 a list of key points indicating joints that can be used for applications such
@@ -28,7 +27,10 @@ def main():
 
     fps = edgeiq.FPS()
 
-    posture = CheckPosture()
+    posture = CheckPosture(scale=0.8)
+
+    bad_posture = False
+    alpha = 0
 
     try:
         with edgeiq.WebcamVideoStream(cam=0) as video_stream, \
@@ -44,19 +46,29 @@ def main():
                 # Generate text to display on streamer
                 text = ["Model: {}".format(pose_estimator.model_id)]
                 text.append(
-                        "Inference time: {:1.3f} s".format(results.duration))
+                    "Inference time: {:1.3f} s".format(results.duration))
                 for ind, pose in enumerate(results.poses):
                     text.append("Person {}".format(ind))
-                    text.append('-'*10)
+                    text.append('-' * 10)
                     # update the instance key_points to check the posture
                     posture.set_key_points(pose.key_points)
-                    
+
                     # play a reminder if you are not sitting up straight
                     correct_posture = posture.correct_posture()
                     if not correct_posture:
-                        text.append(posture.build_message())    
+                        text.append(posture.build_message())
                         # make a sound to alert the user to improper posture
-                        print("\a")
+                        #print("\a")
+                        if bad_posture:
+                            alpha += 0.001
+                            alpha = max(0, min(alpha, 0.5))
+                        else:
+                            bad_posture = True
+                            alpha = 0
+                        overlay(alpha)
+                    else:
+                        overlay(0)
+                        bad_posture = False
 
                 streamer.send_data(results.draw_poses(frame), text)
 
@@ -65,13 +77,20 @@ def main():
                 if streamer.check_exit():
                     break
 
-                #time.sleep(2.0)
+                # time.sleep(2.0)
     finally:
         fps.stop()
         print("elapsed time: {:.2f}".format(fps.get_elapsed_seconds()))
         print("approx. FPS: {:.2f}".format(fps.compute_fps()))
 
         print("Program Ending")
+
+
+def overlay(alpha):
+    label.master.wm_attributes("-alpha", alpha)
+    label.update_idletasks()
+    label.update()
+    time.sleep(.1)
 
 
 if __name__ == "__main__":
@@ -96,5 +115,6 @@ if __name__ == "__main__":
     win32api.SetWindowLong(hWindow, win32con.GWL_EXSTYLE, exStyle)
 
     label.pack()
-    label.mainloop()
+    label.master.wm_attributes("-alpha", 0)
+
     main()
